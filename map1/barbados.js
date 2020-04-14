e5c40f2a51073695a2a85c20c85a2ee3dcd0a340
Ashlee Autore <autorea311@gmail.com>
2020-04-14 18:28:53 -0500

    Updated map coords

--- map1/barbados.js
+++ map1/barbados.js
@@ -1,16 +1,17 @@
-/* global L* /
-var barbMap = L.map('barbadosMap').setView([17, -63], 1)
-L.tileLayer('https://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png?apikey={apikey}').addTo(barbMap)
-var bridgetownMark = L.marker([13.1, -59.5]).addTo(barbMap)
+/* global L */
+var barbMap = L.map('barbadosMap').setView([13.14, -59.5], 12)
+L.tileLayer('https://dev.{s}.tile.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png').addTo(barbMap)
+var bridgetownMark = L.marker([13.115, -59.5999]).addTo(barbMap)
 var coastPoly = L.polygon([
-  [13.2, -59.7],
-  [13, -59.7],
-  [13.1, -59.4],
+  [13.107129, -59.625079],
+  [13.113534, -59.626926],
+  [13.113952, -59.625061],
+  [13.108599, -59.621867]
 ]).addTo(barbMap)
-coastPoly.bindPopup('In this highlighted area you can find...')
-bridgetownMark.bindPopup('Barbados.')
+coastPoly.bindPopup('In this highlighted area you can find Mount Gay Distillery and the house Rhianna grew up in')
+bridgetownMark.bindPopup('Saint Michael Parish.')
 
-//More specific coords if allowed by map:
+// More specific coords if allowed by map:
 // [13.113, -59.627],
 //  [13.114, -59.627],
 //  [13.108, -59.621],
