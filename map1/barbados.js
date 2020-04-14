5666293a11ba1f8895dd370057e74538749cd841
Ashlee Autore <autorea311@gmail.com>
2020-04-14 18:35:11 -0500

    fixed spelling errors

--- map1/barbados.js
+++ map1/barbados.js
@@ -8,11 +8,5 @@ var coastPoly = L.polygon([
   [13.113952, -59.625061],
   [13.108599, -59.621867]
 ]).addTo(barbMap)
-coastPoly.bindPopup('In this highlighted area you can find Mount Gay Distillery and the house Rhianna grew up in')
+coastPoly.bindPopup('In this highlighted area you can find Mount Gay Distillery and the house Rihanna grew up in')
 bridgetownMark.bindPopup('Saint Michael Parish.')
-
-// More specific coords if allowed by map:
-// [13.113, -59.627],
-//  [13.114, -59.627],
-//  [13.108, -59.621],
-//  [13.106, -59.625]
